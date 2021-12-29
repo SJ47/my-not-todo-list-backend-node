745473cc83d671daf14322e7ceca9b49391f63ba
Scott Johnston <scott@umachan.co.uk>
2021-12-29 17:15:08 +0000

    adding header for cors response

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ require("dotenv").config();
 
 app.use(express.json()); //Used to parse JSON bodies instead of line above
 app.use(cors());
-
+app.use((req, res) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    // next();
+});
 const username = process.env.REACT_APP_DB_USER;
 const password = process.env.REACT_APP_DB_KEY;
 
@@ -22,7 +27,7 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     })
     .catch(console.error);
 
-// app.listen(process.env.PORT || 5000, function () {
-//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// });
+app.listen(process.env.PORT || 5000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 // comment to redploy
